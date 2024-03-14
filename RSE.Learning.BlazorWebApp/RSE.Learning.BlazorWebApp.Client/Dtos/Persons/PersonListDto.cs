4384dbd9ea5a9da236835a7d070ca0bf876d014f
Franz Duran <engrparanz@gmail.com>
2024-03-14 15:01:45 +0800

    Add project files.

---
+++ RSE.Learning.BlazorWebApp/RSE.Learning.BlazorWebApp.Client/Dtos/Persons/PersonListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RSE.Learning.BlazorWebApp.Client.Dtos.Persons;
+
+public class PersonListDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Gender { get; set; }
+    public string? Address { get; set; }
+}
